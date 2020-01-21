15350bf7e82986aa1f7e21b90faadd4cceceb8a5
shibataka000 <chise.alter.pasta@gmail.com>
2020-01-21 20:08:05 +0900

    Add testcase

--- internal/dailyreport/parser_test.go
+++ internal/dailyreport/parser_test.go
@@ -12,9 +12,14 @@ type parseTestData struct {
 }
 
 func TestParse(t *testing.T) {
-	testData := []parseTestData{
+	tests := []struct {
+		name string
+		in   string
+		out  dailyReport
+	}{
 		{
-			text: `
+			name: "case01",
+			in: `
 # 日報
 
 ## 業務時間
@@ -34,7 +39,7 @@ func TestParse(t *testing.T) {
 
 # 業務記録
 `,
-			expect: dailyReport{
+			out: dailyReport{
 				timecard: timecard{
 					begin: newTime(9, 30, 0),
 					end:   newTime(17, 30, 0),
@@ -70,13 +75,106 @@ func TestParse(t *testing.T) {
 		},
 	}
 
-	for _, test := range testData {
-		actual, err := parse(test.text)
-		if err != nil {
-			t.Error(err)
-		}
-		if !reflect.DeepEqual(test.expect, actual) {
-			t.Errorf("Expected is %v but actual is %v", test.expect, actual)
-		}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			actual, err := parse(tt.in)
+			if err != nil {
+				t.Error(err)
+			}
+			if !reflect.DeepEqual(actual, tt.out) {
+				t.Errorf("Expected is %v but actual is %v", tt.out, actual)
+			}
+		})
+	}
+}
+
+func TestParseTime(t *testing.T) {
+	tests := []struct {
+		name string
+		in   string
+		out  time.Time
+	}{
+		{
+			name: "- 始業 09:30",
+			in:   "- 始業 09:30",
+			out:  newTime(9, 30, 0),
+		},
+		{
+			name: "- 終業 17:30",
+			in:   "- 終業 17:30",
+			out:  newTime(17, 30, 0),
+		},
+		{
+			name: "- 休憩 01:00",
+			in:   "- 休憩 01:00",
+			out:  newTime(1, 0, 0),
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			actual, err := parseTime(tt.in)
+			if err != nil {
+				t.Error(err)
+			}
+			if actual != tt.out {
+				t.Errorf("Expected is %v but actual is %v", tt.out, actual)
+			}
+		})
+	}
+}
+
+func TestParseTask(t *testing.T) {
+	tests := []struct {
+		name     string
+		category string
+		s        string
+		out      task
+	}{
+		{
+			name:     "- [ ] 1.0h/1.5h タスク",
+			category: "カテゴリ",
+			s:        "- [ ] 1.0h/1.5h タスク",
+			out: task{
+				category:   "カテゴリ",
+				name:       "タスク",
+				expectTime: time.Duration(1) * time.Hour,
+				actualTime: time.Duration(1)*time.Hour + time.Duration(30)*time.Minute,
+			},
+		},
+		{
+			name:     "- [x] 1.0h/1.5h タスク",
+			category: "カテゴリ",
+			s:        "- [x] 1.0h/1.5h タスク",
+			out: task{
+				category:   "カテゴリ",
+				name:       "タスク",
+				expectTime: time.Duration(1) * time.Hour,
+				actualTime: time.Duration(1)*time.Hour + time.Duration(30)*time.Minute,
+			},
+		},
+		{
+			name:     "    - [ ] 1.0h/1.5h タスク",
+			category: "カテゴリ",
+			s:        "    - [ ] 1.0h/1.5h タスク",
+			out: task{
+				category:   "カテゴリ",
+				name:       "タスク",
+				expectTime: time.Duration(1) * time.Hour,
+				actualTime: time.Duration(1)*time.Hour + time.Duration(30)*time.Minute,
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			actual, err := parseTask(tt.s, tt.category)
+			if err != nil {
+				t.Error(err)
+			}
+			if actual != tt.out {
+				t.Errorf("Expected is %v but actual is %v", tt.out, actual)
+			}
+		})
 	}
 }
